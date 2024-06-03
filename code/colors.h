c45fb664bbd1893efe0782a9f749adc19c1208bd
dudykoren <83447907+dudykoren@users.noreply.github.com>
2024-06-03 23:42:08 +0300

    Create colors.h

---
+++ code/colors.h
@@ -0,0 +1,50 @@
+
+#ifndef ANSI_COLORS_H
+#define ANSI_COLORS_H
+
+#define ANSI_RESET       "\033[0m"
+
+// Regular Colors
+#define ANSI_BLACK       "\033[0;30m"
+#define ANSI_RED         "\033[0;31m"
+#define ANSI_GREEN       "\033[0;32m"
+#define ANSI_YELLOW      "\033[0;33m"
+#define ANSI_BLUE        "\033[0;34m"
+#define ANSI_MAGENTA     "\033[0;35m"
+#define ANSI_CYAN        "\033[0;36m"
+#define ANSI_WHITE       "\033[0;37m"
+
+// Bold
+#define ANSI_BOLD_BLACK  "\033[1;30m"
+#define ANSI_BOLD_RED    "\033[1;31m"
+#define ANSI_BOLD_GREEN  "\033[1;32m"
+#define ANSI_BOLD_YELLOW "\033[1;33m"
+#define ANSI_BOLD_BLUE   "\033[1;34m"
+#define ANSI_BOLD_MAGENTA "\033[1;35m"
+#define ANSI_BOLD_CYAN   "\033[1;36m"
+#define ANSI_BOLD_WHITE  "\033[1;37m"
+
+// Underline
+#define ANSI_UNDERLINE_BLACK  "\033[4;30m"
+#define ANSI_UNDERLINE_RED    "\033[4;31m"
+#define ANSI_UNDERLINE_GREEN  "\033[4;32m"
+#define ANSI_UNDERLINE_YELLOW "\033[4;33m"
+#define ANSI_UNDERLINE_BLUE   "\033[4;34m"
+#define ANSI_UNDERLINE_MAGENTA "\033[4;35m"
+#define ANSI_UNDERLINE_CYAN   "\033[4;36m"
+#define ANSI_UNDERLINE_WHITE  "\033[4;37m"
+
+// Background
+#define ANSI_BG_BLACK   "\033[40m"
+#define ANSI_BG_RED     "\033[41m"
+#define ANSI_BG_GREEN   "\033[42m"
+#define ANSI_BG_YELLOW  "\033[43m"
+#define ANSI_BG_BLUE    "\033[44m"
+#define ANSI_BG_MAGENTA "\033[45m"
+#define ANSI_BG_CYAN    "\033[46m"
+#define ANSI_BG_WHITE   "\033[47m"
+
+#endif // ANSI_COLORS_H
+
+
+ 
